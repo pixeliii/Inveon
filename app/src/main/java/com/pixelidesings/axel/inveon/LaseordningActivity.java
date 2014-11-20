23d29f5a9273769fd8e310b65f7f27d9efc8e9ff
Axel Andersin <axel.andersin@gmail.com>
2014-11-20 12:35:23 +0200

    Initial commit

---
+++ app/src/main/java/com/pixelidesings/axel/inveon/LaseordningActivity.java
@@ -0,0 +1,17 @@
+package com.pixelidesings.axel.inveon;
+
+import android.os.Bundle;
+
+/**
+ * Created by axel on 20.11.2014.
+ */
+public class LaseordningActivity extends MainActivity{
+
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_laseordning);
+
+}
+
+
+}
